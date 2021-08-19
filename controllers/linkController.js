2394f624397784ee4647ec38f0e76ab6ae74ea5a
Gabriel Miller <gabrielmillerone@hotmail.com>
2021-08-19 10:57:29 -0300

    Implementing EDIT on system

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -20,14 +20,14 @@ const addLink = async (req, res) => {
         let doc = await link.save();
         res.redirect('/');
     } catch (error) {
-        res.render('index', { error, body: req.body });
+        res.render('add', { error, body: req.body });
     }
 };
 
 const allLinks = async (req, res) => {
     try {
-        let links = await Link.find({});
-        res.render('all', { links });
+        let docs = await Link.find({});
+        res.render('all', { links: docs });
     } catch (error) {
         res.send(error);
     }
@@ -35,7 +35,7 @@ const allLinks = async (req, res) => {
 
 const deleteLink = async (req, res) => {
     let id = req.params.id;
-    if(!id){
+    if (!id) {
         id = req.body.id;
     }
     try {
@@ -45,6 +45,36 @@ const deleteLink = async (req, res) => {
     } catch (error) {
         res.status(404).send(error);
     }
+};
+
+const loadLink = async (req, res) => {
+    let id = req.params.id;
+    try {
+        let doc = await Link.findById(id);
+        res.render('edit', { error: false, body: doc });
+    } catch (error) {
+        res.status(404).send(error);
+    }
+};
+
+const editLink = async (req, res) => {
+    let link = {};
+
+    link.title = req.body.title;
+    link.description = req.body.description;
+    link.url = req.body.url;
+
+    let id = req.params.id;
+    if (!id) {
+        id = req.body.id;
+    }
+
+    try {
+        let doc = await Link.updateOne({_id: id}, link);
+        res.redirect('/');
+    } catch (error) {
+        res.render('edit', { error, body: req.body });
+    }
 }
 
-module.exports = { redirect, addLink, allLinks, deleteLink };
+module.exports = { redirect, addLink, allLinks, deleteLink, loadLink, editLink };
--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -2,19 +2,17 @@ const express = require('express');
 const router = express.Router();
 const linkController = require('../controllers/linkController')
 const methodOverride = require('method-override')
-
 router.use(methodOverride('_method'))
 
 router.get('/', linkController.allLinks);
-
 router.get('/:title', linkController.redirect);
-
-router.get('/add', (req, res) => res.render('index', { error: false, body: {} }));
+router.get('/add', (req, res) => res.render('add', { error: false, body: {} }));
+router.get('/edit/:id', linkController.loadLink);
 
 router.post('/', express.urlencoded({ extended: true }), linkController.addLink);
+router.post('/edit/:id', express.urlencoded({ extended: true }), linkController.editLink);
 
 router.delete('/:id', linkController.deleteLink);
-
 router.delete('/', express.json(), linkController.deleteLink);
 
 module.exports = router;
